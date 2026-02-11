34c4128b957e383f4620a1de96a06e73fed1c141
Vedant Pandey <ved2003ant@gmail.com>
2026-02-11 12:21:03 +0530

    Commiting UC2 feature

--- LineProblem/Program.cs
+++ LineProblem/Program.cs
@@ -3,8 +3,8 @@
     
     public class Point
     {
-        public int X;
-        public int Y;
+        public int X { get; }
+        public int Y { get; }
 
         public Point(int x, int y)
         {
@@ -12,13 +12,41 @@
             this.Y = y;
         }
 
-        public static double LineLength(Point p1, Point p2)
+        
+    }
+
+    public class Line
+    {
+        
+        public Point Start { get; }
+        public Point End { get; }
+        public double Length { get; }
+
+        public Line(Point start,Point end)
         {
-            double Xlength = p2.X - p1.X;
-            double Ylength = p2.Y - p1.Y;
+            Start = start;
+            End = end;
+            Length= LineLength();
+        }
+        public double LineLength()
+        {
+            double Xlength = End.X - Start.X;
+            double Ylength = End.Y - Start.Y;
             double length = 0;
             length = Math.Sqrt(Math.Pow(Xlength, 2) + Math.Pow(Ylength, 2));
             return length;
+        }
+
+        public void LineCompare(Line l1,Line l2)
+        {
+            if (l1.Length.Equals(l2.Length))
+            {
+                Console.WriteLine("Lines are equal");
+            }
+            else
+            {
+                Console.WriteLine("Lines are not equal");
+            }
         }
     }
     internal class Program
@@ -27,6 +55,8 @@
         {
             Console.WriteLine("Welcome To Line Computation");
 
+            //line 1 
+            Console.WriteLine("Enter Line1 Points");
             Console.WriteLine("Enter X and Y values for Point1:");
             Console.Write("X:"); 
             int x1 = int.Parse(Console.ReadLine());
@@ -44,12 +74,40 @@
 
             Console.WriteLine();
 
-            Point Pa = new Point(x1,y1);
-            Point Pb = new Point(x2,y2);
+            Line line1 = new Line(new Point(x1, y1), new Point(x2, y2));
 
-            double length = Point.LineLength(Pa, Pb);
+            Console.WriteLine("Length of Line1 "+  line1.LineLength());
+            Console.WriteLine();
 
-            Console.WriteLine("Length Between Lines"+  length);
+            //line 2
+
+            Console.WriteLine("Enter Line2 Points");
+            Console.WriteLine("Enter X and Y values for Point2:");
+            Console.Write("X:");
+            int x3 = int.Parse(Console.ReadLine());
+            Console.Write("Y:");
+            int y3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+
+            Console.WriteLine("Enter X and Y values for Point2:");
+            Console.Write("X:");
+            int x4 = int.Parse(Console.ReadLine());
+            Console.Write("Y:");
+            int y4 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Line line2 = new Line(new Point(x3, y3), new Point(x4, y4));
+
+            Console.WriteLine("Length Between Line2 " + line2.LineLength());
+            Console.WriteLine();
+
+            // comparing line
+
+            Console.WriteLine("Comparing Lines");
+            line1.LineCompare(line1,line2);
         }
     }
 }
